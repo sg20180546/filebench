11e16bf41c1a9d02bacff5a3976867769a760257
gs201 <sg20180546@gmail.com>
2024-04-12 16:31:12 +0900

    2 phase fileserver

--- workloads/fileserver.f
+++ workloads/fileserver.f
@@ -23,14 +23,14 @@
 # Use is subject to license terms.
 #
 
-set $dir=/tmp
+set $dir=/home/sungjin/f2fs_mount
 set $nfiles=10000
 set $meandirwidth=20
 set $filesize=cvar(type=cvar-gamma,parameters=mean:131072;gamma:1.5)
 set $nthreads=50
 set $iosize=1m
 set $meanappendsize=16k
-set $runtime=60
+set $runtime=10
 
 define fileset name=bigfileset,path=$dir,size=$filesize,entries=$nfiles,dirwidth=$meandirwidth,prealloc=80
 
@@ -53,5 +53,30 @@ define process name=filereader,instances=1
 }
 
 echo  "File-server Version 3.0 personality successfully loaded"
+
+run $runtime
+
+
+echo  "File-server Version 3.0 personality successfully phase 2"
+
+define process name=filereader,instances=1
+{
+  thread name=filereaderthread,memsize=10m,instances=$nthreads
+  {
+    flowop createfile name=createfile1,filesetname=bigfileset,fd=1
+    flowop writewholefile name=wrtfile1,srcfd=1,fd=1,iosize=$iosize
+    flowop closefile name=closefile1,fd=1
+    flowop openfile name=openfile1,filesetname=bigfileset,fd=1
+    flowop appendfilerand name=appendfilerand1,iosize=$meanappendsize,fd=1
+    flowop closefile name=closefile2,fd=1
+    flowop openfile name=openfile2,filesetname=bigfileset,fd=1
+    flowop readwholefile name=readfile1,fd=1,iosize=$iosize
+    flowop closefile name=closefile3,fd=1
+    flowop deletefile name=deletefile1,filesetname=bigfileset
+    flowop statfile name=statfile1,filesetname=bigfileset
+  }
+}
+
+echo  "File-server Version 3.0 personality successfully phase 2 done"
 
 run $runtime
