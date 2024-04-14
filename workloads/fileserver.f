02da99e94187269989154c167087a03a86a59b3e
gs201 <sg20180546@gmail.com>
2024-04-14 17:37:44 +0900

    fiesrever

--- workloads/fileserver.f
+++ workloads/fileserver.f
@@ -23,7 +23,7 @@
 # Use is subject to license terms.
 #
 
-set $dir=/home/sungjin/f2fs_mount
+set $dir=/home/femu/mnt
 set $nfiles=10000
 set $meandirwidth=20
 set $filesize=cvar(type=cvar-gamma,parameters=mean:131072;gamma:1.5)
