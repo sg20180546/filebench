ec1c2aefaa388bfcab93bb36a00e7c156677b987
micron <sg20180546@gmail.com>
2024-08-02 16:45:29 +0900

    32mb,41000

--- workloads/fileserver3.f
+++ workloads/fileserver3.f
@@ -26,11 +26,11 @@
 set mode quit alldone
 
 set $dir=/home/femu/f2fs_mount
-set $nfiles=20000
+set $nfiles=30000
 set $meandirwidth=20
 set $filesize=cvar(type=cvar-gamma,parameters=mean:268435456;gamma:1.5)
 set $nthreads=8
-set $iosize=128m
+set $iosize=32m
 set $readiosize=128m
 set $meanappendsize=2m
 
@@ -61,7 +61,7 @@ define process name=filereader,instances=1
     flowop deletefile name=deletefile1,filesetname=bigfileset
 
     flowop statfile name=statfile1,filesetname=bigfileset
-    flowop finishoncount name=foc,value=11000
+    flowop finishoncount name=foc,value=41000
   }
 }
 
